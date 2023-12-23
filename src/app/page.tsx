e2586670e838f5ca1bda1c55ccbfed47ca8558e4
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-12-23 07:00:32 -0500

    update home page with hero image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,22 @@
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
+    <div
+      className="hero min-h-screen"
+      style={{
+        backgroundImage: 'url(/img/kids.jpg)',
+      }}
+    >
+      <div className="hero-overlay bg-opacity-60"></div>
+      <div className="hero-content text-center text-neutral-content">
+        <div className="max-w-md">
+          <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+          <p className="mb-5">Welcome to the kids chores app</p>
+          <button type="button" aria-label="button" className="btn btn-primary">
+            Get Started
+          </button>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
