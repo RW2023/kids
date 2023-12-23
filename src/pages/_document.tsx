5646018c5eedcfc8c9f1d880688f56c539a10aa2
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-12-23 14:08:38 -0500

    Add _document.tsx for Next.js custom document configuration

---
+++ src/pages/_document.tsx
@@ -0,0 +1,22 @@
+// _document.tsx src/pages/_document.tsx
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta charSet="utf-8" />
+          <meta name="description" content="Kids Chores App" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
