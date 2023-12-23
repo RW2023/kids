ef7e881a4d000badb47d66c33654a4c13551e985
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-12-23 09:46:03 -0500

    Add imports for Heading and SubHeading components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,22 @@
-
+import Heading from '@/components/ui/Heading';
+import SubHeading from '@/components/ui/SubHeading';
 
 export default function Home() {
   return (
     <div
       className="hero min-h-screen"
-      style={{
-        backgroundImage: 'url(/img/kids.jpg)',
-      }}
+      // style={{
+      //   backgroundImage: 'url(/img/kids.jpg)',
+      // }}
     >
       <div className="hero-overlay bg-opacity-60"></div>
       <div className="hero-content text-center text-neutral-content">
         <div className="max-w-md">
-          <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-          <p className="mb-5">Welcome to the kids chores app</p>
+          <Heading title="Kids Chores" iconClass="fas fa-child" />
+          <SubHeading
+            title="track your kids chores"
+            iconClass="fas fa-tasks"
+          />{' '}
           <button type="button" aria-label="button" className="btn btn-primary">
             Get Started
           </button>
