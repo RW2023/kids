8a370d2a628a32130f302fd2452cbb32e77dc07e
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-12-24 02:44:54 -0500

    Refactor page transition animation in _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,14 @@
 // pages/_app.js
 import type { AppProps } from 'next/app';
 import Layout from '@/app/layout';
-import { AnimatePresence, motion } from 'framer-motion';
+import { AnimatePresence } from 'framer-motion';
 
 function MyApp({ Component, pageProps, router }: AppProps) {
   return (
     <Layout>
       {/* AnimatePresence with mode="wait" for page transitions */}
       <AnimatePresence mode="wait">
-        {/* Ensure key is present for proper page transitions */}
-        <motion.div key={router.route}>
-          <Component {...pageProps} />
-        </motion.div>
+        <Component {...pageProps} key={router.route} />
       </AnimatePresence>
     </Layout>
   );
