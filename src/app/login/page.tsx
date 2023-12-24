7f0e5c41f6fa7a461ae462ebffc2881fb8ce6d4d
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-12-23 21:01:02 -0500

    Refactor login page component

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
-import SubHeading from '@/components/ui/SubHeading'; // assuming you have a SubHeading component
+import SubHeading from '@/components/ui/SubHeading';
 
 const Login = () => {
   return (
-    <div className="hero min-h-screen bg-base-200">
+    <div className="hero min-h-screen bg-base-200 text-lg">
       <div className="hero-content flex-col lg:flex-row-reverse">
         <div className="text-center lg:text-left">
           <h1 className="text-5xl font-bold">Login now!</h1>
-          <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
-          </p>
+          <div className="p-2">
+            <SubHeading
+              title="   Access your personalized dashboard and take control of your
+              household chores effortlessly. !"
+              iconClass="fas fa-sign-in-alt"
+            />
+          </div>
         </div>
         <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
           <form className="card-body">
