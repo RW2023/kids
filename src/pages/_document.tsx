bbaaf1d687eec933bc717a232ed95fc09298027e
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-12-25 14:47:11 -0500

    Remove meta description tag from _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,6 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <meta charSet="utf-8" />
-          <meta name="description" content="Kids Chores App" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
         </Head>
         <body>
