0af3521669a286e635dbb0f013b9ff1c986e4df3
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-12-25 18:27:53 -0500

    Add meta tag for theme color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en" data-theme="dim">
       <head>
+        <meta name="theme-color" content="#ACC6CF" />
         <link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
