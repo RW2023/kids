a65be0b4a39cb24a48d89456cb96071b98390058
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-12-25 09:42:45 -0500

    Add Chore model to src/models/chore.ts

--- src/models/chore.ts
+++ src/models/chore.ts
@@ -1,5 +1,16 @@
-//src/models/chores.ts
 import mongoose, { Schema, model, Document } from 'mongoose';
+
+// Interface to define the type for a Subtask Document
+interface ISubtask extends Document {
+  title: string;
+  status: 'pending' | 'completed';
+}
+
+// Subtask Schema
+const subtaskSchema: Schema = new Schema({
+  title: { type: String, required: true },
+  status: { type: String, enum: ['pending', 'completed'], default: 'pending' },
+});
 
 // Interface to define the type for a Chore Document
 interface IChore extends Document {
@@ -8,14 +19,17 @@ interface IChore extends Document {
   assignedTo: mongoose.Types.ObjectId;
   status: 'pending' | 'completed';
   rewardPoints: number;
+  subtasks: ISubtask[];
 }
 
+// Chore Schema
 const choreSchema: Schema = new Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
   assignedTo: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   status: { type: String, enum: ['pending', 'completed'], default: 'pending' },
   rewardPoints: { type: Number, default: 0 },
+  subtasks: [subtaskSchema], // Adding the subtasks array
 });
 
 const Chore = model<IChore>('Chore', choreSchema);
