721185fc54a4812e3d7b04b554f6084d14f2924f
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-12-25 18:27:39 -0500

    Add service worker for installation and activation

---
+++ public/service-worker.js
@@ -0,0 +1,9 @@
+self.addEventListener('install', (event) => {
+  console.log('Service Worker installing.');
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('Service Worker activating.');
+});
+
+// Add event listeners for other service worker events as needed
