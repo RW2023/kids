efb1a87f818b1fcd233b8105df50dcf435fa436d
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-12-25 18:28:01 -0500

    Add service worker registration in _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,26 @@
-// pages/_app.js
 import type { AppProps } from 'next/app';
 import Layout from '@/app/layout';
 import { AnimatePresence } from 'framer-motion';
+import React, { useEffect } from 'react'; // Import useEffect from React
 
 function MyApp({ Component, pageProps, router }: AppProps) {
+  useEffect(() => {
+    // Register the service worker on mount
+    if ('serviceWorker' in navigator && process.env.NODE_ENV !== 'production') {
+      navigator.serviceWorker
+        .register('/service-worker.js')
+        .then((registration) => {
+          console.log(
+            'Service Worker registered with scope:',
+            registration.scope,
+          );
+        })
+        .catch((error) => {
+          console.error('Error during service worker registration:', error);
+        });
+    }
+  }, []);
+
   return (
     <Layout>
       {/* AnimatePresence with mode="wait" for page transitions */}
