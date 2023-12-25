a6bf1ffb31dfa7f8d86bf4cde8d4caeb09f49593
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-12-24 19:45:28 -0500

    Update home page content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,10 +17,10 @@ const HomePage = () => {
         <div className="hero-content text-center text-neutral-content">
           <div className="max-w-md">
             <HomeHeading title="Chore tracker" iconClass="fas fa-home" />
+            <HomeSubHeading title='"I will pull my weight!"' />
             <HomeSubHeading
-              title='"I will pull my weight!"'            />
-            <HomeSubHeading
-              title="you can only track what you measure" iconClass='fas fa-chart-bar'
+              title="you can only track what you measure"
+              iconClass="fas fa-chart-bar"
             />
             <Link href={'/login'}>
               <button
@@ -43,15 +43,27 @@ const HomePage = () => {
             <div className="card bg-base-300 shadow-xl">
               <div className="card-body">
                 <h2 className="card-title">Organize Tasks</h2>
-                <p>Efficiently categorize and track your household chores.</p>
+                <p>
+                  <i className="fas fa-clipboard-list mr-2"></i>Efficiently
+                  categorize and track your household chores.
+                </p>
+                <p>
+                  <i className="fas fa-weight-hanging mr-2"></i>See who is
+                  pulling their weight and who is slacking!
+                </p>
               </div>
             </div>
 
             <div className="card bg-base-300 shadow-xl">
               <div className="card-body">
-                <h2 className="card-title">Set Reminders</h2>
+                <h2 className="card-title">Set Reminders and Notification</h2>
                 <p>
-                  Never forget a task with timely reminders and notifications.
+                  <i className="fas fa-bell mr-2"></i>No more nagging! Set
+                  reminders and notifications to keep everyone on track.
+                </p>
+                <p>
+                  <i className="fas fa-bell mr-2"></i>Get notified when tasks
+                  are completed.
                 </p>
               </div>
             </div>
@@ -60,8 +72,12 @@ const HomePage = () => {
               <div className="card-body">
                 <h2 className="card-title">Collaborate</h2>
                 <p>
-                  Share tasks and collaborate with family members for a
-                  well-managed home.
+                  <i className="fas fa-user-plus mr-2"></i>Add and update users
+                  and chores
+                </p>
+                <p>
+                  <i className="fas fa-user-friends mr-2"></i>Invite all family
+                  members to collaborate.
                 </p>
               </div>
             </div>
@@ -76,10 +92,12 @@ const HomePage = () => {
             iconClass="fas fa-question-circle"
           />
 
-          <p className="max-w-lg mx-auto">
-            ChoreMaster is designed to bring harmony and efficiency to your
-            household chores. With our easy-to-use interface and powerful
-            features, managing daily tasks has never been easier.
+          <p className="max-w-lg mx-auto text-xl">
+            Chore Tracker is designed to root out slackers. It is a simple and
+            easy to use chore tracker that allows you to track who is pulling
+            their weight and who is slacking. It is designed to be used by the
+            whole family. It is a great way to teach kids about responsibility
+            and accountability.{' '}
           </p>
         </div>
       </div>
