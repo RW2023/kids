333d7c23631e8c91ed3dbe98be53133c276f5c95
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-12-25 14:47:46 -0500

    Remove unnecessary meta tags from layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,8 +22,6 @@ export default function RootLayout({ children }: RootLayoutProps) {
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
         />
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
       <body>
         {!isHomepage && <Navbar />}
