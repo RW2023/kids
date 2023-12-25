b2489e6021dacd4e398b4ec0e1eaccd7196f76f8
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-12-25 10:38:23 -0500

    add Endpoint for Updating a Chore

---
+++ src/pages/api/chores/[id].ts
@@ -0,0 +1,26 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import dbConnect from '@/utils/mongodb';
+import Chore from '@/models/chore';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  const { id } = req.query;
+
+  await dbConnect();
+
+  if (req.method === 'PUT') {
+    try {
+      const chore = await Chore.findByIdAndUpdate(id, req.body, { new: true });
+      if (!chore) {
+        return res.status(404).json({ error: 'Chore not found' });
+      }
+      res.status(200).json(chore);
+    } catch (error) {
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  } else {
+    res.status(405).json({ error: 'Method not allowed' });
+  }
+}
