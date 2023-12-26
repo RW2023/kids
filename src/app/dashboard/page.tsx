373092b26eb6b44e6daac9cc053d384077282618
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-12-26 05:15:32 -0500

    Refactor DashboardPage component to fetch and display chores

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,12 +1,45 @@
 'use client';
-import { FC } from 'react';
+import React, { useState, useEffect } from 'react';
 import SubHeading from '@/components/ui/SubHeading';
 import { motion } from 'framer-motion';
+import { fetchChores } from '@/services/choreService';
+import Loading from '@/components/ui/Loading';
 
-interface Props {}
+interface Subtask {
+  title: string;
+  status: string;
+  // Add other fields as needed
+}
 
-const DashboardPage: FC<Props> = (): JSX.Element => {
-  // Define motion variants for page transitions
+interface Chore {
+  _id: string;
+  title: string;
+  description: string;
+  status: string;
+  rewardPoints: number;
+  subtasks: Subtask[];
+  __v: number;
+}
+
+const DashboardPage: React.FC = () => {
+  const [chores, setChores] = useState<Chore[]>([]);
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const loadChores = async () => {
+      try {
+        const data = await fetchChores();
+        setChores(data);
+      } catch (error) {
+        console.error('Error fetching chores:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    loadChores();
+  }, []);
+
   const pageTransitionVariants = {
     initial: { opacity: 0, x: -200 },
     animate: { opacity: 1, x: 0 },
@@ -15,37 +48,40 @@ const DashboardPage: FC<Props> = (): JSX.Element => {
 
   return (
     <>
-    <head>
-      <title>Dashboard</title>
-      <meta name="description" content="Chore Tracker dashboard/login" />
-    </head>
-    <motion.div
-      initial="initial"
-      animate="animate"
-      exit="exit"
-      variants={pageTransitionVariants}
-      className="container mx-auto p-6 min-h-screen"
-    >
-      <SubHeading title="Dashboard" iconClass="fas fa-tachometer-alt" />
+      <head>
+        <title>Dashboard</title>
+        <meta name="description" content="Chore Tracker dashboard/login" />
+      </head>
+      <motion.div
+        initial="initial"
+        animate="animate"
+        exit="exit"
+        variants={pageTransitionVariants}
+        className="container mx-auto p-6 min-h-screen"
+      >
+        <SubHeading title="Dashboard" iconClass="fas fa-tachometer-alt" />
 
-      <div className="grid grid-cols-1 gap-6 lg:grid-cols-2">
-        {/* Widget for Task Overview */}
-        <div className="card bg-base-300 shadow-xl glass text-lg p-6">
-          <h2 className="text-xl font-semibold">Task Overview</h2>
-          {/* Display task-related information here */}
-        </div>
-
-        {/* Widget for Rewards */}
-        <div className="card bg-base-300 shadow-xl glass text-lg p-6">
-          <h2 className="text-xl font-semibold">Rewards</h2>
-          {/* Display reward-related information here */}
-        </div>
-
-        {/* Additional widgets or cards can be added here */}
-      </div>
-
-      {/* Other dashboard components or sections */}
-    </motion.div>
+        {isLoading ? (
+          <Loading />
+        ) : (
+          <div className="grid grid-cols-1 gap-6 lg:grid-cols-2">
+            {chores.map((chore) => (
+              <div
+                key={chore._id}
+                className="card bg-base-300 shadow-xl glass text-lg p-6"
+              >
+                <h2 className="text-xl font-semibold">{chore.title}</h2>
+                <p>{chore.description}</p>
+                <ul>
+                  {chore.subtasks.map((subtask, index) => (
+                    <li key={index}>{subtask.title}</li>
+                  ))}
+                </ul>
+              </div>
+            ))}
+          </div>
+        )}
+      </motion.div>
     </>
   );
 };
