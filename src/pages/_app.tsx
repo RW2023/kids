0743a1043632f92d7b2f63f96ff5c5c955022318
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-12-26 04:11:04 -0500

    Comment out service worker console log

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,10 +10,10 @@ function MyApp({ Component, pageProps, router }: AppProps) {
       navigator.serviceWorker
         .register('/service-worker.js')
         .then((registration) => {
-          console.log(
-            'Service Worker registered with scope:',
-            registration.scope,
-          );
+          // console.log(
+          //   'Service Worker registered with scope:',
+          //   registration.scope,
+          // );
         })
         .catch((error) => {
           console.error('Error during service worker registration:', error);
