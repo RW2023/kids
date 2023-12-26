825a19d255c0d5a80c1a3b1423be793147c92e3a
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-12-26 03:58:52 -0500

    Add og:image meta tag to _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,18 @@
-// _document.tsx src/pages/_document.tsx
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import getBaseUrl from '@/utils/getBaseUrl'; // Assuming getBaseUrl is the function name
 
 class MyDocument extends Document {
   render() {
+    const baseUrl = getBaseUrl(); // Call the function to get the base URL
+    const ogImageUrl = `${baseUrl}/path/to/your/og-image.jpg`; // Construct the full OG image URL
+
     return (
       <Html lang="en">
         <Head>
           <meta charSet="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta property="og:image" content={ogImageUrl} />{' '}
+          {/* Use the full URL */}
         </Head>
         <body>
           <Main />
