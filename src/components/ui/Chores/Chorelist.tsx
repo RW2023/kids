c6948a110fc8c0d6a63c4f79081f7e369ff4e0a0
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2024-03-31 09:49:36 -0400

    Create chore List component

---
+++ src/components/ui/Chores/Chorelist.tsx
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from 'react';
+import { supabase } from '@/utils/supabaseClient'; // Adjust the import path as necessary
+import { Database } from '@/lib/database.types'; // Adjust the import path as necessary
+import Chore from './Chore'; // Make sure this path points to your Chore component
+
+const ChoreList: React.FC = () => {
+  const [chores, setChores] = useState<
+    (Database['public']['Tables']['chores']['Row'] & {
+      subtasks: Database['public']['Tables']['subtasks']['Row'][];
+    })[]
+  >([]);
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchChoresAndSubtasks = async () => {
+      setLoading(true);
+      try {
+        let { data: choresData, error } = await supabase.from('chores').select(`
+          id,
+          title,
+          description,
+          status,
+          created_at,
+          updated_at,
+          user_id,
+          subtasks:subtasks!inner(*)
+        `);
+
+        if (error) {
+          throw error;
+        }
+
+        // Ensures that choresData is never null when setting the state
+        setChores(choresData || []);
+      } catch (error) {
+        console.error('Error fetching chores:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchChoresAndSubtasks();
+  }, []);
+
+  return (
+    <div>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        chores.map((chore) => <Chore key={chore.id} chore={chore} />)
+      )}
+    </div>
+  );
+};
+
+export default ChoreList;
