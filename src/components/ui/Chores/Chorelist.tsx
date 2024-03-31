c97e7e23d579d9567ebac5cf229c045a268eab98
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2024-03-31 13:32:49 -0400

    Refactor ChoreList component layout. make grid render 3x3 on large and tablet and stacked on mobile

--- src/components/ui/Chores/Chorelist.tsx
+++ src/components/ui/Chores/Chorelist.tsx
@@ -44,7 +44,7 @@ const ChoreList: React.FC = () => {
   }, []);
 
   return (
-    <div>
+     <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-3 gap-4">
       {isLoading ? (
         <Loading />
       ) : (
