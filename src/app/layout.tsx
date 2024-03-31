9e01ba07850ae333a7a4c872b03a15c306c86dec
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2024-03-31 17:36:00 -0400

    Update data-theme value in layout.tsx. set to 'dim' for now

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
   const isHomepage = pathname === '/';
 
   return (
-    <html lang="en" data-theme="business">
+    <html lang="en" data-theme="dim">
       <head>
         <title>Chore Tracker</title>
         <meta
