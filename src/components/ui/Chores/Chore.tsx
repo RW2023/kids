52f3ffb3cd037cca53934e6c7c8f7a86aedc4553
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2024-03-31 17:36:51 -0400

    Refactor Chore component UI. remove hero for now

--- src/components/ui/Chores/Chore.tsx
+++ src/components/ui/Chores/Chore.tsx
@@ -1,6 +1,7 @@
-'use client'
+'use client';
 import React from 'react';
-import { Database } from '@/lib/database.types'; 
+import { Database } from '@/lib/database.types';
+import SubHeading from '../SubHeading';
 
 // Define the props for the Chore component using the structure of your database types
 interface ChoreProps {
@@ -11,27 +12,33 @@ interface ChoreProps {
 
 const Chore: React.FC<ChoreProps> = ({ chore }) => {
   return (
-    <div className="bg-base-200 rounded-box border-2 p-4 m-4 relative shadow-lg">
-      <div className="font-bold text-xl mb-2">{chore.title}</div>
-      <p>{chore.description}</p>
-      <div>
-        Status: <span className="badge badge-primary">{chore.status}</span>
+    <div className="bg-base-200 rounded border-2 p-4 m-4 relative shadow-lg card-compact">
+      <div className="title">
+        <SubHeading title={chore.title} />
       </div>
-      <div>
-        Created at: <span className="badge badge-info">{chore.created_at}</span>
-      </div>
-      <div>
-        Updated at: <span className="badge badge-info">{chore.updated_at}</span>
-      </div>
-      <div className="mt-4">
-        <h3 className="underline">Subtasks:</h3>
-        <ul>
-          {chore.subtasks.map((subtask) => (
-            <li key={subtask.id}>
-              {subtask.title} - {subtask.status}
-            </li>
-          ))}
-        </ul>
+      <div className="card-body">
+        <p>{chore.description}</p>
+        <div>
+          Status: <span className="badge badge-error">{chore.status}</span>
+        </div>
+        <div>
+          Assigned:{' '}
+          <span className="badge badge-info ">{chore.created_at}</span>
+        </div>
+        <div>
+          Last Updated:{' '}
+          <span className="badge badge-info">{chore.updated_at}</span>
+        </div>
+        <div className="mt-4">
+          <h3 className="underline">Subtasks:</h3>
+          <ul>
+            {chore.subtasks.map((subtask) => (
+              <li key={subtask.id} className="list-disk list-inside">
+                {subtask.title} - {subtask.status}
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
   );
